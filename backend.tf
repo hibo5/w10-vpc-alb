0fa0e6d84d3fbdfa7ce2cb663ac35ba4578e8a02
Hibo <hibboahmed1989@outlook.com>
2024-09-23 12:52:28 -0500

    terraform week10

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "w7-hy-terraform" #replace with yourbucket
+    bucket         = "hjjaassskkkk" #replace with yourbucket
     key            = "week10/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "locktable"
+    dynamodb_table = "jhhfdnjmkk"
   }
 }
